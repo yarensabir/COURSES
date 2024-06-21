340465c448eddc068fb9a413de554747ae39d644
yarensabir <yaren.s.ae@gmail.com>
2024-06-21 15:11:58 +0300

    not eklendi

--- btkakademi.gov.tr/VERİ_BİLİMİ_İÇİN_PYTHON_VE_TENSORFLOW/paketleme/anaPaket/__init__.py
+++
@@ -1,2 +0,0 @@
-# içinde hiçbir şey olmasa bile __init__.py dosyasını koyduğumuz dizini python paket kabul eder
-
--- btkakademi.gov.tr/VERİ_BİLİMİ_İÇİN_PYTHON_VE_TENSORFLOW/paketleme/yeniProje.py
+++ btkakademi.gov.tr/VERİ_BİLİMİ_İÇİN_PYTHON_VE_TENSORFLOW/paketleme/yeniProje.py
@@ -1,3 +1,6 @@
+# içinde hiçbir şey olmasa bile __init__.py dosyasını koyduğumuz dizini python paket kabul eder
+# __pycache__ klasörleri otomatik oluştu
+
 from anaPaket import modul1
 from anaPaket.alt_paket import modul2
 
